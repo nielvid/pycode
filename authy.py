d6b8a6fcefbb9338960c880298948f983ad65c45
nielvid <>
2021-04-19 23:53:10 +0100

    code refactored

--- authy.py
+++ authy.py
@@ -3,7 +3,7 @@ import random
 # -  Register
 # _ username, email, password
 
-# - userid  - gnerete user account
+# - userid  - generate user account
 # - Login
 # - (username or email), password
 
@@ -15,10 +15,10 @@ database = {}
 
 def init():
     print('Welcome to bankPHP')
-    haveAccount = int(input("Do you have account with us> 1(yes) 2(no) \n"))
-    if haveAccount == 1:
+    have_account = int(input("Do you have account with us> 1(yes) 2(no) \n"))
+    if have_account == 1:
         login()
-    elif haveAccount == 2:
+    elif have_account == 2:
         register()
 
     else:
@@ -27,18 +27,18 @@ def init():
 
 
 def register():
-    surName = input("What is your Surname \n ")
-    firstName = input("What is your Firstname \n ")
+    surname = input("What is your Surname \n ")
+    firstname = input("What is your Firstname \n ")
     email = input("What is your email address \n ")
     username = input("Choose a username \n")
     password = input("Choose a password \n")
-    accountNumber = generate_account_number()
+    account_number = generate_account_number()
 
-    database[accountNumber] = {"surname": surName, "firstname": firstName, "username": username, "email": email,
-                               "password": password, "accountnumber": accountNumber}
+    database[account_number] = {"surname": surname, "firstname": firstname, "username": username, "email": email,
+                                "password": password, "account_number": account_number}
     print('Your account has been created')
 
-    details = database[accountNumber]
+    details = database[account_number]
     print(details)
     login()
 
@@ -50,17 +50,17 @@ def generate_account_number():
 def login():
     print("**************** LOGIN ****************")
 
-    account_number_inputed = int(input('Enter you account number \n '))
+    account_number_input = int(input('Enter you account number \n '))
     password = input("What is your password \n ")
 
     for accountNumber, items in database.items():
         print(accountNumber)
     for item in items:
-        acc_no = items['accountnumber']
-        dbuserpassword = items['password']
-        if acc_no == account_number_inputed:
-            if dbuserpassword == password:
-                print('LOGIN SUCCESSFULL')
+        acc_no = items['account_number']
+        user_password = items['password']
+        if acc_no == account_number_input:
+            if user_password == password:
+                print('LOGIN SUCCESSFUL')
                 transaction()
     print('Invalid account number or password')
     login()
@@ -68,9 +68,20 @@ def login():
 
 def transaction():
     print('What would you like to do')
-    print('Deposit')
-    seletOneOption = int(input('Select 1 or 2 or 3 or 4 \n'))
-    print("you seleceted %s" %seletOneOption)
+    print('To make Deposit press 1')
+    print('To make Withdrawal press 3')
+    print('To exit press 4')
+    selected_option = int(input('Select an option \n'))
+    if selected_option == 1:
+        def deposit():
+            pass
+    elif selected_option == 2:
+        def withdraw_cash():
+            pass
+    elif selected_option == 3:
+        def check_balance():
+            pass
+    print("you selected %s" % selected_option)
 
 
 # ACTUAL BANKING SYSTEM
